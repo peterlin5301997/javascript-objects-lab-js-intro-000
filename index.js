0b2abdaac6253e25bcb4b3e7d00fbd6691d645d0
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-06 22:34:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = new Object({ cake: "eggs", "flour"});
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  Object.assign({}, object, {key: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var test = Object.assign({}, object);
+  delete test[key];
+  return test;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
